ab673cec87fe353fd37dd8450b21a7f8d67c9f64
Wesley Moraes <wesleyjava88@gmail.com>
2022-04-15 00:45:58 -0300

    implement-controller-with-routes

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,36 @@
+import { Controller } from "./controller.js";
+import config from "./config.js";
+import { logger } from "./util.js";
+
+const controller = new Controller();
+
+
+async function routes(request, response) {
+
+    const { method, url } = request
+
+    if (method === 'GET' && url === '/') {
+        response.writeHead(302, {
+            'Location': config.location.home
+        })
+
+        return response.end();
+    }
+
+    if (method === 'GET' && url === '/home') {
+        
+        const { stream } = await controller.getFileStream(config.pages.homeHTML)
+        //pardrão do response é text/html
+        // response.writeHead(200,{
+        // 'Content-Type' : 'text/html'
+        //})
+        return stream.pipe(response)
+    }
+
+    return response.end('hello!')
+}
 
 export function handler(request, response) {
-    return response.end('hello');
+    return routes(request, response)
+        .catch(error => logger.error(`Deu ruimmm: ${error.stack}`));
 }
--- server/service.js
+++ server/service.js
@@ -2,13 +2,12 @@ import fs from 'fs';
 import { join, extname } from 'path';
 import config from './config.js';
 import fsPromises from 'fs/promises'
-import { type } from 'os';
 
 const { dir: { publicDirectory } } = config;
 
 export class Service {
     createFileStream(filename) {
-        return fs.createFileStream(filename)
+        return fs.createReadStream(filename)
     }
 
     async getFileInfo(file) {
@@ -26,14 +25,16 @@ export class Service {
     }
 
     async getFileStream(file) {
-        const { 
+        const {
             name,
             type
         } = await this.getFileInfo(file)
 
         return {
+
             stream: this.createFileStream(name),
-           
+            type
+
         }
     }
 }

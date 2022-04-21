7391560619d919c2fedc88b59e83c87c3fa1d880
Wesley Moraes <wesleyjava88@gmail.com>
2022-04-21 16:02:56 -0300

    test-GET/-complete

--- test/unit/public/routes.test.js
+++ test/unit/public/routes.test.js
@@ -1,8 +1,11 @@
 import { jest, expect, describe, test, beforeEach } from '@jest/globals'
 import config from '../../../server/config.js'
+import TestUtil from '../_util/testUtil';
+import { handler } from '../../../server/routes.js';
 
 const {
-    pages
+    pages,
+    location
 } = config;
 
 
@@ -10,6 +13,22 @@ describe('#Routes - test site for api response', () => {
     beforeEach(() => {
         jest.restoreAllMocks();
         jest.clearAllMocks();
+    })
+
+    test('GET / -should redirect to home page', async () => {
+        const params = TestUtil.defaultHandleParams()
+        params.request.method = 'GET';
+        params.request.url = '/'
+
+        await handler(...params.values());
+
+        expect(params.response.writeHead).toBeCalledWith(
+            302,
+            {
+                'Location': location.home
+            }
+        )
+        expect(params.response.end).toHaveBeenCalled();
     })
 
     test.todo('GET / - should redirect to home page')
